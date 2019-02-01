6c1a0aadc2f0a3a5b23198440ab67efe0206199c
Owen Watson <owen_watson84@hotmail.com>
2019-02-01 23:01:49 +0000

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// init window
+let win;
+
+function createWindow(){
+
+    // Create browser window
+    win = new BrowserWindow({width: 800, height: 600, icon: __dirname+'./img/avatar.png'});
+
+    // Load the index.html file
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // open dev tools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    })
+}
+
+// Run create window
+app.on('ready', createWindow);
+
+// Quit when all windows closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
